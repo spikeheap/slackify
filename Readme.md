3e11c5049c8549ae0cd5e894483371962be7e215
Ryan Brooks <ryanbrooksis@gmail.com>
2016-09-05 10:33:24 +0100

    WORKING

--- Readme.md
+++ Readme.md
@@ -1,4 +1,42 @@
 ## To do
+- ORM
+- protect all management pages with OAuth
 
-- Support multiple playlists: user auths Slack and Spotify
-- Connection objects 1:1 Spotify => Slack
+- one user account has one spotify account and one slack account
+- user logs in with spotify or slack credentials
+- list of all connections for that user
+
+accounts
+  - email
+
+logins
+  - provider
+  - token
+  - refresh_token
+  - expires_at
+  - expires (boolean)
+
+connections
+  - validation_token
+  - spotify_login_id
+  - playlist_owner_id
+  - playlist_id # must belong to login owner.
+
+
+### Installation
+
+Add a `.env` with your credentials:
+
+```
+# Slack credentials
+SLACK_TOKEN=
+
+# Spotify credentials
+SPOTIFY_CLIENT_ID=
+SPOTIFY_CLIENT_SECRET=
+
+# Playlist
+SPOTIFY_PLAYLIST_USER=
+SPOTIFY_PLAYLIST_ID=
+
+```
--- app.rb
+++ app.rb
@@ -12,8 +12,18 @@ configure do
 end
 
 use OmniAuth::Builder do
-  provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'], scope: 'user-read-email playlist-modify-public' # user-library-read user-library-modify
+  provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'], scope: 'user-read-email playlist-modify-public playlist-read-collaborative playlist-modify-private' # user-library-read user-library-modify
 end
+
+# # id and credentials are required to prime RSpotify
+# id = request.env['omniauth.auth']['info']['id']
+# credentials = request.env['omniauth.auth']['credentials']
+
+# RSpotify::User.new(id: id, credentials: credentials)
+
+# # these might be handy
+# # display_name = request.env['omniauth.auth']['info']['display_name']
+# # email = request.env['omniauth.auth']['info']['email']
 
 
 get '/' do
@@ -23,7 +33,21 @@ get '/' do
 end
 
 get '/auth/:name/callback' do
-  @@spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+  # persist the login
+  # auth_data = request.env['omniauth.auth']
+
+  # Login.create!(
+  #     account: Account.find_or_create_by(email: auth_data['info']['email']),
+  #     provider: 'spotify',
+  #     token: auth_data['credentials']['token'],
+  #     refresh_token: auth_data['credentials']['refresh_token'],
+  #     expires_at: auth_data['credentials']['expires_at'],
+  #     expires: true
+  #   )
+
+  # creating the user object primes the global credentials cache
+  RSpotify::User.new(request.env['omniauth.auth'])
+
   <<-HTML
     <h1>Linked!</h1>
     <a href="/auth/spotify">Do it again</a>
@@ -57,13 +81,22 @@ post '/webhooks/slack' do
 end
 
 
+def get_playlist(playlist_owner, id)
+  url = "users/#{playlist_owner}/"
+  url << (id == 'starred' ? id : "playlists/#{id}")
+
+  response = RSpotify::User.oauth_get(playlist_owner, url)
+  return response if RSpotify.raw_response
+  RSpotify::Playlist.new response
+end
+
 def extract_song_ids_from(text)
   text.scan(/(?:https:\/\/open.spotify.com\/track\/|spotify:track:)([a-zA-Z0-9]+)/).flatten
 end
 
 def add_to_spotify_playlist(playlist_owner, playlist_id, tracks)
-  binding.pry
-  playlist = RSpotify::Playlist.find(playlist_owner, playlist_id)
+  playlist = get_playlist(playlist_owner, playlist_id)
+  #playlist = RSpotify::Playlist.find(playlist_owner, playlist_id)
   playlist.add_tracks!(tracks)
 
   puts "Added #{tracks.map(&:name).join(', ')} to playlist '#{playlist.name}'"
